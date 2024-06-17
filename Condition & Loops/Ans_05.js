408a593e9c75cc89559a52c876146aa832d8eeec
Dev78600 <104481878+Dev78600@users.noreply.github.com>
2024-06-17 15:04:41 +0530

    Add files via upload

---
+++ Condition & Loops/Ans_05.js
@@ -0,0 +1,15 @@
+// ASCENDING ORDER CODE
+let i = 1;
+console.log("ASCENDING ORDER: ")
+while(i <= 25){
+    console.log(i);
+    i++;
+}
+
+// DESCENDING ORDER CODE
+let j = 25;
+console.log("DESCENDING ORDER: ")
+while(j >= 1){
+  console.log(j);
+  j--;
+}
